de4a2a5a4b60eea33ee560a36ee89c7b7f5a985a
djmcflush <demarcus.edwards@bison.howard.edu>
2023-05-07 11:32:15 -0700

    remove checkpoints from repo

---
+++ .checkpoints/download_checkpoints.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Google Drive file ID
+FILE_ID="1oruqzflcmPRTdLx71jt7BNgQ_SKx_7Dc"
+# Desired file name and extension
+FILE_NAME="rave_pretrained.ckpt"
+
+FILE_ID2="1yPYuF3vk1Ge-0NwX0tP3lOJyfjNKdwW7"
+FILE_NAME2="latest_rave_pretrained.ckpt"
+
+# Create the .checkpoints directory if it doesn't exist
+mkdir -p .checkpoints
+
+# Download the file from Google Drive and save it in the .checkpoints folder
+wget --no-check-certificate "https://drive.google.com/uc?id=${FILE_ID}&export=download" -O ".checkpoints/${FILE_NAME}"
+wget --no-check-certificate "https://drive.google.com/uc?id=${FILE_ID2}&export=download" -O ".checkpoints/${FILE_NAME2}"
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # MusicClip
 A Clip model that maps music to text
+
+
+./download_checkpoints.sh
